d411531b790fc89d4fdf1c32acbf329db0b9fc33
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-10-30 06:01:37 -0400

    commit tree 6f3c4d3c481130fbeb01aa75977cb5533d21c017

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,50 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			errors: [],
+			user: null,
+			settings: {
+				bitrate: 8,
+				video: {
+					resolution: '1080p'
+				}
+			}
+		}
+	}
+
+	changeBitrate = () => {
+		this.setState({
+			settings: {
+				...this.state.settings,
+				bitrate: 12
+			}
+		}, () => console.log(this.state.settings.bitrate))
+	}
+
+	changeResolution = () => {
+		this.setState({
+			settings: {
+				...this.state.settings,
+				video: {
+					resolution: '720p'
+				}
+			}
+		}, () => console.log(this.state.settings.video.resolution))
+	}
+
+	render() {
+		return(
+			<div>
+				<button onClick={this.changeBitrate} className='bitrate'>Change bitrate</button>
+				<button onClick={this.changeResolution} className='resolution'>Change resolution</button>
+			</div>
+		)
+	}
+}
+
+export default YouTubeDebugger
